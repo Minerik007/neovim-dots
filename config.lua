e2f2cd1ba8ecf2b2b5072c261f7b5f80881094be
Minerik007 <minerik007@gmail.com>
2024-11-11 14:53:18 +0100

    fixed wraping and images

--- config.lua
+++ config.lua
@@ -10,6 +10,17 @@ vim.opt.clipboard = "unnamedplus"
 lvim.colorscheme = "catppuccin-mocha"
 lvim.transparent_window = true
 
+-- Enable soft wrapping for Markdown files
+lvim.autocommands = {
+    {
+        "CursorMoved",
+        {
+            pattern = {"*.md", "*.txt"},
+            command = "setlocal wrap linebreak nolist textwidth=0",
+        }
+    },
+}
+
 lvim.plugins = {
     { "lunarvim/colorschemes" },
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
@@ -42,7 +53,6 @@ lvim.plugins = {
             },
         },
     },
-
     {
         "jakobkhansen/journal.nvim",
         config = function()
@@ -65,15 +75,6 @@ lvim.plugins = {
                 }
             })
         end,
-    },
-
-    {
-        'andrewferrier/wrapping.nvim',
-        config = function()
-            require('wrapping').setup({
-                filetypes = { "text", "markdown", "latex" },
-            })
-        end
     },
 
     --[[{
@@ -125,4 +126,40 @@ require'lspconfig'.gdscript.setup{
 }
 
 
+require("image").setup({
+  backend = "kitty",
+  processor = "magick_rock", -- or "magick_cli"
+  integrations = {
+    markdown = {
+      enabled = true,
+      clear_in_insert_mode = true,
+      download_remote_images = true,
+      only_render_image_at_cursor = true,
+      filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+    },
+    neorg = {
+      enabled = true,
+      filetypes = { "norg" },
+    },
+    typst = {
+      enabled = true,
+      filetypes = { "typst" },
+    },
+    html = {
+      enabled = false,
+    },
+    css = {
+      enabled = false,
+    },
+  },
+  max_width = nil,
+  max_height = nil,
+  max_width_window_percentage = nil,
+  max_height_window_percentage = 50,
+  window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+  window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+  editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+  tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+  hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+})
 
