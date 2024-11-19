8212434b5aa8f23b8691effc1c98b5d0113bab49
Minerik007 <minerik007@gmail.com>
2024-11-19 14:29:26 +0100

    added bufferline.nvim error and warning indicators

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,7 +5,12 @@ return {
     config = function()
         require("bufferline").setup{
             options = {
-                separator_style = "slope"
+                separator_style = "slope",
+                diagnostics = "nvim_lsp",
+                diagnostics_indicator = function(count, level, diagnostics_dict, context)
+                    local icon = level:match("error") and " " or " "
+                    return " " .. icon .. count
+                end,
             }
         }
     end
