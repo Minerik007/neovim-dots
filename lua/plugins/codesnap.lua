e5629cc002becdf825a8cdb286d2936d4ffcc54f
Minerik007 <minerik007@gmail.com>
2025-01-04 14:29:44 +0100

    added CodeSnap.nvim plugin

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,11 @@
+return {
+    "mistricky/codesnap.nvim",
+    build = "make",
+    opts = {
+        code_font_family = "Lilex Nerd Font",
+        has_breadcrumbs = true,
+        show_workspace = true,
+        has_line_number = true,
+        bg_theme = "summer"
+    }
+}
