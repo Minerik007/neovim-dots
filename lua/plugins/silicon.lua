026c00a7f1cc416877fbadcb515fd0b267b713e7
Minerik007 <minerik007@gmail.com>
2025-01-05 17:03:37 +0100

    replaced codesnap with silicon

---
+++ lua/plugins/silicon.lua
@@ -0,0 +1,19 @@
+return {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    main = "nvim-silicon",
+    opts = {
+        font = "Lilex Nerd Font;Lilex",
+        theme = "Catppuccin Mocha",
+        to_clipboard = true,
+        background = "#11111b",
+        shadow_color = "#000000",
+        window_title = function()
+            return vim.fn.fnamemodify(
+                vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()),
+                ":t"
+            )
+        end,
+    }
+}
