83b95c40922ad4c2b772ca597e9140c666854ceb
Minerik007 <minerik007@gmail.com>
2025-03-30 18:19:23 +0200

    fix

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -41,7 +41,7 @@ return {
         dashboard.section.header.opts.hl = "Directory"
         dashboard.section.buttons.val = {
             dashboard.button('n', '  New file', ':ene <BAR> startinsert <CR>'),
-            dashboard.button('b', '󰷏 File browser', ':Telescope file_browser <CR>'),
+            dashboard.button('b', '󰷏  File browser', ':Telescope file_browser <CR>'),
             dashboard.button('f', '  Find file', ':Telescope find_files <CR>'),
             dashboard.button('t', '  Find text', ':Telescope live_grep <CR>'),
             dashboard.button('r', '󰄉  Recent files', ':Telescope oldfiles <CR>'),
